a747b64ade039fc27d8402a70decb408bd018376
Sungmin Baik <bsm050704@gmail.com>
2024-09-30 19:22:13 -0500

    test

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,13 +1,23 @@
+import React from "react";
 
 function App() {
+  
+  const handleClick = () => {
+    alert('Button Clicked!');
+  };
 
   return (
-    <div className="w-[100vw] h-[100vh] bg-white">
-      <p className="h-full w-16 m-auto">
+    <div className="w-[100vw] h-[100vh] bg-white flex flex-col justify-center items-center">
+      <p className="text-center">
         Welcome to the website
       </p>
+        <div className="App">
+        <h1>My Button Example</h1>
+        <button onClick={handleClick}>Click Me</button>
+      </div>
     </div>
+    
   )
 }
 
-export default App
+export default App;
